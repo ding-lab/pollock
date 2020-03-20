6dab537299b58deaec5f45964dd887c13ed755af
Erik <epstorrs@gmail.com>
2020-03-20 00:19:04 +0000

    maybe working but need to do more testing

--- pollock/models/model.py
+++ pollock/models/model.py
@@ -6,6 +6,7 @@ import os
 import random
 import re
 import shutil
+import time
 import uuid
 from collections import Counter, Iterable
 
@@ -19,23 +20,30 @@ import scipy
 import scanpy as sc
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder, MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
 
 from tensorflow.keras.models import Sequential
 import tensorflow as tf
 
-import pollock.preprocessing.preprocessing as pollock_pp
-import pollock.models.analysis as pollock_analysis
+## import pollock.preprocessing.preprocessing as pollock_pp
+#import pollock.models.model.analysis as pollock_analysis
+from pollock import get_confusion_matrix
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 MODEL_PATH = 'model.h5' 
 CELL_TYPES_PATH = 'cell_types.npy' 
+GENES_PATH = 'genes.npy' 
 MODEL_SUMMARY_PATH = 'summary.json' 
+STANDARD_SCALER_PATH = 'standard_scaler.pkl' 
+RANGE_SCALER_PATH = 'range_scaler.pkl' 
+ENCODER_PATH = 'encoder.pkl' 
+CLASSIFIER_PATH = 'clf.pkl'
 
 def cap_list(ls, n=100):
     if len(ls) > n:
         return random.sample(ls, n)
-    return ls
+    return random.sample(ls, int(len(ls) * .8))
 
 ## def balanced_adata_filter(adata, cell_type_key, n=1000):
 ##     cell_type_to_idxs = {}
@@ -64,10 +72,11 @@ def balancedish_training_generator(adata, cell_type_key, n_per_cell_type):
                          for k, ls in cell_type_to_idxs.items()}
     
     train_ids = np.asarray([x for ls in cell_type_to_idxs.values() for x in ls])
-    val_ids = np.delete(np.asarray(adata.obs.index), train_ids)
+    train_idxs = np.arange(adata.shape[0])[np.isin(np.asarray(adata.obs.index), train_ids)]
+    val_idxs = np.delete(np.arange(adata.shape[0]), train_idxs)
 
-    train_adata = adata[train_ids, :]
-    val_adata = adata[val_ids, :]
+    train_adata = adata[train_idxs, :]
+    val_adata = adata[val_idxs, :]
 
     return train_adata, val_adata
 
@@ -107,12 +116,12 @@ def process_from_counts(adata, min_genes=200, min_cells=3, mito_threshold=.2, ma
         sc.pp.filter_genes(adata, min_cells=min_cells)
     
     if mito_threshold is not None or max_n_genes is not None: 
-        mito_genes = tumor_adata.var_names.str.startswith('MT-')
+        mito_genes = adata.var_names.str.startswith('MT-')
         if 'sparse' in str(type(adata.X)):
             adata.obs['percent_mito'] = np.sum(
                 adata[:, mito_genes].X, axis=1).A1 / np.sum(adata.X, axis=1).A1
             adata.obs['n_counts'] = adata.X.sum(axis=1).A1
-        else
+        else:
             adata.obs['percent_mito'] = np.sum(
                 adata[:, mito_genes].X, axis=1) / np.sum(adata.X, axis=1)
             # add the total counts per cell as observations-annotation to adata
@@ -123,9 +132,10 @@ def process_from_counts(adata, min_genes=200, min_cells=3, mito_threshold=.2, ma
         if max_n_genes is not None:
             adata = adata[adata.obs.n_genes < max_n_genes, :]
 
-    if cpm:
-        sc.pp.normalize_total(adata, target_sum=1e6)
+##     if cpm:
+##         sc.pp.normalize_total(adata, target_sum=1e6)
     if log:
+        print('here')
         sc.pp.log1p(adata)
     adata.raw = adata
     
@@ -139,7 +149,7 @@ def process_from_counts(adata, min_genes=200, min_cells=3, mito_threshold=.2, ma
         standard_scaler = StandardScaler(with_mean=False, with_std=True)
         adata.X = standard_scaler.fit_transform(adata.X)
     else:
-        adata.X = standadrd_scaler.transofmr(adata.X)
+        adata.X = standard_scaler.transform(adata.X)
 
     if range_scaler is None:
         range_scaler = MinMaxScaler()
@@ -147,13 +157,13 @@ def process_from_counts(adata, min_genes=200, min_cells=3, mito_threshold=.2, ma
     else:
         adata.X = range_scaler.transform(adata.X)
 
-    return adata
+    return adata, standard_scaler, range_scaler
 
 class PollockDataset(object):
     def __init__(self, adata, cell_type_key='ClusterName', n_per_cell_type=500,
             batch_size=64, dataset_type='training', min_genes=200, min_cells=3, mito_threshold=.2,
             max_n_genes=None, log=True, cpm=True, min_disp=.2, standard_scaler=None,
-            range_scaler=None, cell_type_encoder=None, genes=None):
+            range_scaler=None, cell_type_encoder=None, genes=None, cell_types=None):
 
         self.adata = adata
         self.dataset_type = dataset_type
@@ -190,52 +200,54 @@ class PollockDataset(object):
     def set_training_datasets(self):
         """"""
         logging.info(f'normalizing counts for model training')
-        self.adata = process_from_counts(self.adata,
+        self.adata, self.standard_scaler, self.range_scaler = process_from_counts(self.adata,
                 min_genes=self.min_genes, min_cells=self.min_cells, mito_threshold=self.mito_threshold,
                 max_n_genes=self.max_n_genes, log=self.log, cpm=self.cpm, min_disp=self.min_disp,
                 standard_scaler=self.standard_scaler, range_scaler=self.range_scaler)
         self.genes = np.asarray(self.adata.var.index)
 
         logging.info(f'creating datasets')
-        self.train_adata, self.val_data = balancedish_training_generator(self.adata,
+        self.train_adata, self.val_adata = balancedish_training_generator(self.adata,
                 self.cell_type_key, self.n_per_cell_type)
 
-        self.train_ds, self.val_ds = get_training_tf_dataset(train_adata, val_adata,
+        self.train_ds, self.val_ds = get_tf_datasets(self.train_adata, self.val_adata,
                 train_buffer=10000, batch_size=self.batch_size)
 
         self.train_cell_ids = np.asarray(self.train_adata.obs.index)
         self.val_cell_ids = np.asarray(self.val_adata.obs.index)
 
         self.y_train = np.asarray(self.train_adata.obs[self.cell_type_key])
-        self.y_train = self.cell_type_encoder.transform(self.y_train.reshape(-1, 1)).flatten()
+        self.y_train = self.cell_type_encoder.fit_transform(self.y_train.reshape(-1, 1)).flatten()
         self.y_val = np.asarray(self.val_adata.obs[self.cell_type_key])
         self.y_val = self.cell_type_encoder.transform(self.y_val.reshape(-1, 1)).flatten()
 
     def set_prediction_dataset(self):
         logging.info(f'normalizing counts for model training')
-        self.adata = self.adata[:, self.genes]
-        self.adata = process_from_counts(self.adata,
+        self.prediction_adata = self.adata[:, self.genes]
+        self.prediction_adata, _, _ = process_from_counts(self.prediction_adata,
                 min_genes=self.min_genes, min_cells=self.min_cells, mito_threshold=self.mito_threshold,
                 max_n_genes=self.max_n_genes, log=self.log, cpm=self.cpm, min_disp=self.min_disp,
                 standard_scaler=self.standard_scaler, range_scaler=self.range_scaler)
-        self.prediction_ds = get_tf_prediction_ds(self.adata, batch_size=1000)
+        self.prediction_ds = get_tf_prediction_ds(self.prediction_adata, batch_size=1000)
 
 def load_from_directory(adata, model_filepath, batch_size=64):
-    model = tf.keras.models.load_model(os.path.join(model_filepath, MODEL_PATH))
+##     model = tf.saved_model.load(os.path.join(model_filepath, MODEL_PATH))
     cell_types = np.load(os.path.join(model_filepath, CELL_TYPES_PATH), allow_pickle=True)
     genes = np.load(os.path.join(model_filepath, GENES_PATH), allow_pickle=True)
     summary = json.load(open(os.path.join(model_filepath, MODEL_SUMMARY_PATH)))
     standard_scaler = joblib.load(os.path.join(model_filepath, STANDARD_SCALER_PATH))
     range_scaler = joblib.load(os.path.join(model_filepath, RANGE_SCALER_PATH))
     encoder = joblib.load(os.path.join(model_filepath, ENCODER_PATH))
+    clf = joblib.load(os.path.join(model_filepath, CLASSIFIER_PATH))
 
     prediction_dataset = PollockDataset(adata, batch_size=batch_size, dataset_type='prediction',
             min_genes=None, min_cells=None, mito_threshold=None,
             max_n_genes=None, log=True, cpm=True, min_disp=None, standard_scaler=standard_scaler,
-            range_scaler=range_scaler, genes=genes, encoder=encoder)
+            range_scaler=range_scaler, genes=genes, cell_type_encoder=encoder,
+            cell_types=cell_types)
 
-    pollock_model = PollockModel(cell_types, input_shape=len(genes), img_width=gene_template.shape[1],
-            img_height=gene_template.shape[0], model=model, summary=summary)
+    pollock_model = PollockModel(cell_types, input_shape=len(genes),
+            model=os.path.join(model_filepath, MODEL_PATH), summary=summary, clf=clf)
 
     return prediction_dataset, pollock_model
 
@@ -243,6 +255,8 @@ class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
+        if isinstance(obj, np.float32):
+            return float(obj)
         return json.JSONEncoder.default(self, obj)
 
 class BVAE(tf.keras.Model):
@@ -275,13 +289,15 @@ class BVAE(tf.keras.Model):
         if eps is None:
             eps = tf.random.normal(shape=(100, self.latent_dim))
         return self.decode(eps, apply_sigmoid=True)
-    
+
     def encode(self, x):
         mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
         return mean, logvar
     
+    @tf.function
     def reparameterize(self, mean, logvar):
         eps = tf.random.normal(shape=mean.shape)
+#        eps = tf.random.normal(shape=mean.shape)
         return eps * tf.exp(logvar * .5) + mean
     
     def decode(self, z, apply_sigmoid=False):
@@ -292,7 +308,19 @@ class BVAE(tf.keras.Model):
     
         return logits
 
-optimizer = tf.keras.optimizers.Adam(1e-4)
+##     def predict(self, x):
+##         print(x.shape)
+##         mean, logvar = self.encode(x)
+##         z = self.reparameterize(mean, logvar)
+##         x_logit = self.decode(z)
+##         return x_logit
+
+##     def call(self, x):
+##         mean, logvar = self.encode(x)
+##         #z = self.reparameterize(mean, logvar)
+##         x_logit = self.sample()
+##         return x_logit
+
 
 def log_normal_pdf(sample, mean, logvar, raxis=1):
     log2pi = tf.math.log(2. * np.pi)
@@ -322,10 +350,10 @@ def compute_apply_gradients(model, x, optimizer, alpha=.00005):
 class PollockModel(object):
     def __init__(self, class_names, input_shape, model=None, learning_rate=1e-4, summary=None, alpha=.1,
             latent_dim=100, clf=RandomForestClassifier()):
-        if model is None:
-            model = BVAE(latent_dim, input_shape)
-        else:
-            self.model = model
+        tf.keras.backend.clear_session()
+        self.model = BVAE(latent_dim, input_shape)
+        if model is not None:
+            self.model.load_weights(model)
 
         self.class_names = class_names
         self.summary = summary
@@ -337,8 +365,16 @@ class PollockModel(object):
         self.val_losses = []
 
     def get_cell_embeddings(self, ds):
-        mean, logvar = self.model.encode(ds)
-        return self.model.reparameterize(mean, logvar).numpy()
+        embeddings = None
+        for X in ds:
+            mean, logvar = self.model.encode(X)
+            emb = self.model.reparameterize(mean, logvar).numpy()
+            if embeddings is None:
+                embeddings = emb
+            else:
+                embeddings = np.concatenate((embeddings, emb), axis=0)
+
+        return embeddings
 
     def fit(self, pollock_dataset, epochs=10):
         for epoch in range(1, epochs + 1):
@@ -352,7 +388,13 @@ class PollockModel(object):
                 loss(compute_loss(self.model, test_x, alpha=self.alpha))
 
             logging.info(f'epoch: {epoch}, val loss: {loss.result()}') 
-            self.val_losses.append(loss.result())
+            self.val_losses.append(loss.result().numpy())
+
+##         ## you need to call model.predict so it saves correctly later
+##         for x in pollock_dataset.train_ds:
+##             #self.model.predict(x)
+##             self.model._set_inputs(x)
+##             break
 
         X_train = self.get_cell_embeddings(pollock_dataset.train_ds)
 
@@ -376,14 +418,15 @@ class PollockModel(object):
         X = self.get_cell_embeddings(ds)
         return self.clf.predict_proba(X)
 
-    def save(self, pollock_training_dataset, filepath, X_val=None, y_val=None,
-            X_train=None, y_train=None, metadata=None):
+    def save(self, pollock_training_dataset, filepath, score_train=True,
+            score_val=True, metadata=None):
         ## create directory if does not exist
         if not os.path.isdir(filepath):
             os.mkdir(filepath)
 
         model_fp = os.path.join(filepath, MODEL_PATH)
-        self.model.save(model_fp)
+##         tf.saved_model.save(self.model, model_fp)
+        self.model.save_weights(model_fp)
         np.save(os.path.join(filepath, CELL_TYPES_PATH),
                 np.asarray(pollock_training_dataset.cell_types))
         np.save(os.path.join(filepath, GENES_PATH),
@@ -391,43 +434,48 @@ class PollockModel(object):
         joblib.dump(pollock_training_dataset.standard_scaler, os.path.join(filepath, STANDARD_SCALER_PATH))
         joblib.dump(pollock_training_dataset.range_scaler, os.path.join(filepath, RANGE_SCALER_PATH))
         joblib.dump(pollock_training_dataset.cell_type_encoder, os.path.join(filepath, ENCODER_PATH))
+        joblib.dump(self.clf, os.path.join(filepath, CLASSIFIER_PATH))
 
         if metadata is not None:
             d = metadata
+        else:
+            d = {}
 
         d['history'] = {'validation_losses': self.val_losses}
 
-        if X_val is not None and y_val is not None:
-            d['validation'] = self.generate_report_for_dataset(X_val, y_val)
+        if score_train:
+            d['training'] = self.generate_report_for_dataset(
+                    pollock_training_dataset.train_ds,
+                    pollock_training_dataset.y_train)
 
-        if X_train is not None and y_train is not None:
-            d['training'] = self.generate_report_for_dataset(X_train, y_train)
+        if score_val:
+            d['training'] = self.generate_report_for_dataset(
+                    pollock_training_dataset.val_ds,
+                    pollock_training_dataset.y_val)
 
         self.summary = d
 
         json.dump(d, open(os.path.join(filepath, MODEL_SUMMARY_PATH), 'w'),
                 cls=NumpyEncoder)
 
-    def generate_report_for_dataset(self, X, y):
-        probs = self.predict(X)
+    def generate_report_for_dataset(self, ds, y):
+        probs = self.predict(ds)
         predictions = np.argmax(probs, axis=1).flatten()
         predicted_labels = [self.class_names[i] for i in predictions]
+        groundtruth = np.asarray([int(i) for i in y])
+        groundtruth_labels = [self.class_names[i] for i in groundtruth]
 
-        cell_type_to_index = {v:k for k, v in enumerate(self.class_names)}
-        groundtruth = [cell_type_to_index[cell_type]
-                for cell_type in y]
+        report = classification_report(groundtruth,
+                predictions, target_names=self.class_names, output_dict=True)
 
-        report = classification_report(groundtruth, predictions,
-               target_names=self.class_names, output_dict=True)
-
-        c_df = pollock_analysis.get_confusion_matrix(predictions,
+        c_df = get_confusion_matrix(predictions,
                 groundtruth, self.class_names, show=False)
 
         d = {
             'metrics': report,
             'probabilities': probs,
             'prediction_labels': predicted_labels,
-            'groundtruth_labels': y,
+            'groundtruth_labels': groundtruth_labels,
             'confusion_matrix': c_df.values,
             }
         return d

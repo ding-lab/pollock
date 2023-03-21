c91d52a242258d4745c74d9f63ef384e226c67d5
Erik Storrs <epstorrs@gmail.com>
2023-03-21 14:43:07 -0500

    fixing anndata conversion error

--- pollock/wrappers/h5ad_to_rds.R
+++ pollock/wrappers/h5ad_to_rds.R
@@ -8,5 +8,5 @@ h5_fp = args[2]
 h5ad_fp = args[3]
 
 Convert(h5ad_fp, dest = "h5seurat", overwrite = TRUE, verbose = TRUE)
-obj <- LoadH5Seurat(h5_fp, verbose = TRUE)
+obj <- LoadH5Seurat(h5_fp, verbose = TRUE, meta.data = FALSE, misc = FALSE)
 saveRDS(obj, file = rds_fp)
